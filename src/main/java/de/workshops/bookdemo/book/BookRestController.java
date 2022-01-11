7998713728626e49614d547b03c07c7e02d43893
Dominik Hirt <dominik@hub28.de>
2022-01-11 10:44:30 +0100

    task: implement simple search

--- src/main/java/de/workshops/bookdemo/book/BookRestController.java
+++ src/main/java/de/workshops/bookdemo/book/BookRestController.java
@@ -2,6 +2,7 @@ package de.workshops.bookdemo.book;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.annotation.PostConstruct;
 
@@ -10,24 +11,27 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping(path = "/book")
+@RequiredArgsConstructor
 public class BookRestController {
     
     private final ObjectMapper mapper;
-
+    
     private final ResourceLoader resourceLoader;
 
     private List<Book> books;
-    
-    public BookRestController(ObjectMapper mapper, ResourceLoader resourceLoader) {
-        this.mapper = mapper;
-        this.resourceLoader = resourceLoader;
-    }
-    
+  
+
     @PostConstruct
     public void init() throws IOException {
         final var resource = resourceLoader.getResource("classpath:books.json");
@@ -37,5 +41,31 @@ public class BookRestController {
     @GetMapping
     public List<Book> getAllAbooks() {
         return this.books;
+    }
+
+    @GetMapping("/{isbn}")
+    public Book getSingleBook(@PathVariable String isbn) throws Exception {
+        return this.books.stream().filter(book -> hasIsbn(book, isbn)).findFirst().orElseThrow(Exception::new);
+    }
+
+    @GetMapping(params = "author")
+    public Book searchBookByAuthor(@RequestParam String author) throws Exception {
+        return this.books.stream().filter(book -> hasAuthor(book, author)).findFirst().orElseThrow(Exception::new);
+    }
+
+    @PostMapping("/search")
+    public List<Book> searchBooks(@RequestBody BookSearchRequest request) {
+        return this.books.stream()
+                .filter(book -> hasAuthor(book, request.getAuthor()))
+                .filter(book -> hasIsbn(book, request.getIsbn()))
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    private boolean hasIsbn(Book book, String isbn) {
+        return book.getIsbn().equals(isbn);
+    }
+
+    private boolean hasAuthor(Book book, String author) {
+        return book.getAuthor().contains(author);
     }
 }
---
+++ src/main/java/de/workshops/bookdemo/book/BookSearchRequest.java
@@ -0,0 +1,9 @@
+package de.workshops.bookdemo.book;
+
+import lombok.Data;
+
+@Data
+public class BookSearchRequest {
+    private String author;
+    private String isbn;
+}
